3cefdeb2980752b1852f9bfb3021e45a574cd9ec
Luan Petruitis <luanpetruitis@hotmail.com>
2020-11-13 16:44:45 -0300

    layout

--- AcelerarAudio/content.js
+++ AcelerarAudio/content.js
@@ -3,15 +3,39 @@ const interval = setInterval(() => {
     if (header) {
         clearInterval(interval)
 
-        const button = document.createElement("button")
-        button.innerHTML = "2x"
+        const button = document.createElement("select")
+        button.innerHTML = `
+            <option value="0.25">0.25</option>
+            <option value="1">0.5</option>
+            <option value="1">1</option>
+            <option value="1.5">1.5</option>
+            <option value="2">2</option>
+            `
         button.classList.add("twoTimesButton")
 
-        button.addEventListener('click', () => {
+        button.addEventListener('change', () => {
             const audios = document.querySelectorAll("audio");
-            audios.forEach((audio) => {
-                audio.playbackRate = 2;
-            })
+            if (document.querySelector('.twoTimesButton').value == 1) {
+                audios.forEach((audio) => {
+                    audio.playbackRate = 1;
+                })
+            } else if (document.querySelector('.twoTimesButton').value == 0.25) {
+                audios.forEach((audio) => {
+                    audio.playbackRate = 0.25;
+                })
+            } else if (document.querySelector('.twoTimesButton').value == 0.5) {
+                audios.forEach((audio) => {
+                    audio.playbackRate = 0.5;
+                })
+            } else if (document.querySelector('.twoTimesButton').value == 1.5) {
+                audios.forEach((audio) => {
+                    audio.playbackRate = 1.5;
+                })
+            } else {
+                audios.forEach((audio) => {
+                    audio.playbackRate = 2;
+                })
+            }
         })
 
         header.appendChild(button)
